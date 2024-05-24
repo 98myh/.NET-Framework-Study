d3cf52c723745eda271b604f8f51ad58cc232f54
myh98 <90823418+Munyounghyun@users.noreply.github.com>
2024-05-24 21:00:39 +0900

    .NET 공부2

---
+++ App/App/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using App.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace App.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            //ViewBag.Arr = new string[]
+            //{
+            //    "alpha",
+            //    "beta",
+            //    "gamma"
+            //};
+
+            ViewBag.Book = new Book
+            {
+                Title = "칼의 노래",
+                Writer = "김훈"
+            };
+
+            return View();
+        }
+    }
+}
